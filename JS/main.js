cc10ff48233e3c804b8deca2737c9eaf9d9f2447
olimjonovibrohim <ib0130nik@gmail.com>
2024-08-22 13:19:14 +0500

    JS 2-dars
    Zeromax IT Center

---
+++ JS/main.js
@@ -0,0 +1,85 @@
+// if ("salom" == "salom"){
+//     console.log("ular teng!");
+// }
+// else{
+//     console.log("ular teng emas!");
+// }
+// if(44 <= 45){
+//     console.log("ular teng yoki katta!")
+// }
+// else{
+//     console.log("ular teng yoki kichik!")
+// }
+// if(5*5 == 5*5){
+//     console.log("ular teng!")
+// }
+// else{
+//     console.log("ular teng emas!")
+// }
+// if("sakra" === "sakra"){
+//     console.log("ular teng!")
+// }
+// else{
+//     console.log("ular teng emas!")
+// }
+// if(45*45 == "45*45"){
+//     console.log("ular teng!")
+// }
+// else{
+//     console.log("ular teng emas!")
+// }
+// if(539*6344 == 344*539){
+//     console.log("1-togri!")
+// }
+// else if(432-333 == 432-333){
+//     console.log("2-togri!")
+// }
+// else{
+//     console.log("3-tugri!")
+// }
+// if("A"=="D"){
+//     console.log("1-togri!")
+// }
+// else if("C"=="B"){
+//     console.log("2-togri!")
+// }
+// else{
+//     console.log("3-tugri!")
+// }
+let a = 46;
+let b = 10;
+let c = 19;
+let d = 49;
+// 
+// if(a == 46 && b == 10 && c == 19 && d == 48){
+//     console.log("hammasi tugri")
+// }
+// else{
+//     console.log("kamida 1ta notogri")
+// }
+// if(a == 6 || b == 0 || c == 9 || d == 4){
+//     console.log("kamida 1ta tugri")
+// }
+// else{
+//     console.log("hammasi notogri")
+// }
+if(a == 46 && b == 10 && c == 19 && d == 49){
+    console.log("hammasi tugri")
+}
+else{
+    console.log("kamida 1ta notogri")
+}
+if("aha" || "hah" || "ha" || "haha"){
+    console.log("hammasi yo kamida 1ta tugri")
+}
+else{
+    console.log("hammasi notogri")
+}
+
+
+
+
+
+
+
+
